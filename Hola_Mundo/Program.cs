f5c3af3a38532db2f6d1b943ac39e72253da3c6c
Garu006 <garojas@uamv.edu.ni>
2024-08-19 18:20:51 -0600

    primer ejercicio

---
+++ Hola_Mundo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HolaMundo
+{
+    internal class Program
+    { 
+        static void Main(string[]agrs)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
