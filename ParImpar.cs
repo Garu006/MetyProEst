189791788c347017ec36c20e2ee55833a48f5238
Garu006 <garojas@uamv.edu.ni>
2024-08-21 07:29:04 -0600

    Par e Impar

---
+++ ParImpar.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Ingrese un numero: ");
+int num = int.Parse(Console.ReadLine());
+if(num%2==0)
+{
+    Console.WriteLine("Este numero " + num + " es par");
+}
+else
+{
+    Console.WriteLine("Este numero " + num + " no es par");
+}
+
