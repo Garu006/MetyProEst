136e43a4ccaa43157041b96f6d4fb05b69ea2112
Garu006 <garojas@uamv.edu.ni>
2024-08-21 07:21:53 -0600

    Area de un circulo

---
+++ AreaCirculo.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+double radio;
+double area;
+Console.WriteLine("Ingrese el radio: ");
+radio = Double.Parse(Console.ReadLine());
+
+area = radio * radio * Math.PI;
+Console.WriteLine("El area del cirulo es: " + area);
