5159e841a096d9816e7ac18a95c801d073f1020a
Garu006 <garojas@uamv.edu.ni>
2024-08-23 17:10:08 -0600

    ejemplos de la clase del 23 de agosto

---
+++ ejemplos/Notafinal/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Notafinal.clases;
+
+namespace EstudianteNotaFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo 2");
+            Nota nota = new Nota();
+            int nota1 = 0;
+            int nota2 = 0, notaFinal;
+            int nota3 = 0;
+
+            Console.WriteLine("Ingrese la primera nota: ");
+            nota1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la segunda nota: ");
+            nota2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la tercena nota: ");
+            nota3 = int.Parse(Console.ReadLine());
+
+            nota.Nota1 = nota1;
+            nota.Nota2 = nota2;
+            nota.Nota3 = nota3;
+
+            notaFinal = nota.calcularNota();
+
+
+            Console.WriteLine($"{notaFinal}");
+        }
+    }
+}
---
+++ ejemplos/Notafinal/clases/Nota.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notafinal.clases
+{
+    internal class Nota
+    {
+        public int Nota1 { get; set; }
+        public int Nota2 { get; set; }
+        public int Nota3 { get; set; }
+
+        public int calcularNota()
+        {
+            return (this.Nota1 + this.Nota2 + this.Nota3) / 3;
+        }
+        public Nota()
+        {
+
+        }
+    }
+
+}
+
+    
+
