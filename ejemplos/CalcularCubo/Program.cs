e99549a68369190766c153461fd6a60ed1ec23d9
Garu006 <garojas@uamv.edu.ni>
2024-08-24 06:45:51 -0600

    Calcular el cubo de un numero

---
+++ ejemplos/CalcularCubo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CalcularCubo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, resultado;
+            Console.WriteLine("Ingrese el numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            resultado = num1 * num1 * num1;
+            Console.WriteLine("El numero " + num1 + "elevado al cubo es: " + resultado);
+        }
+    }
+}
---
+++ ejemplos/CalcularCubo/clases/cubo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularCubo.clases
+{
+    internal class cubo
+    {
+        public int num1 {  get; set; }
+        public int resutado()
+        {
+            return num1 * num1 * num1;
+        }
+    }
+}
