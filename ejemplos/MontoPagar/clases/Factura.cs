ff1b1ba2fca61e83f6399f0c0c7d532ad97ae8a6
Garu006 <garojas@uamv.edu.ni>
2024-08-24 06:18:01 -0600

    monto a pagar

---
+++ ejemplos/MontoPagar/clases/Factura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace MontoPagar.clases
+{
+    internal class Factura
+    {
+        //get: otiene el valor
+        //set: asigna ese valor
+        public string Producto { get; set; }
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+        public double calcularMonto()
+        {
+            return this.Precio * this.Cantidad;
+        }
+        //los constructores son las maneras para crear los objetos
+        public Factura()
+        {
+
+        } 
+    }   
+}
