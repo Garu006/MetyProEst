8b4b1020a64dff7bfb862d85aefe6f84f364ef33
Garu006 <garojas@uamv.edu.ni>
2024-08-26 09:15:07 -0600

    Calcular el area de un circulo

---
+++ ejemplos/AreaCirculodos/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AreaCirculodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio;
+            double area;
+            Console.WriteLine("Ingrese el radio: ");
+            radio = Double.Parse(Console.ReadLine());
+
+            area = radio * radio * Math.PI;
+            Console.WriteLine("El area del cirulo es: " + area);
+        }
+    }
+}
---
+++ ejemplos/AreaCirculodos/clases/areaCirculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCirculodos.clases
+{
+    internal class areaCirculo
+    {
+        double radio {  get; set; } 
+        double area { get; set; }
+
+        public double calcular()
+        {
+            return this.radio=this.radio*this.radio*Math.PI;
+        }
+    }
+}
