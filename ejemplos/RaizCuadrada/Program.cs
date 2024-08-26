411157820ee9d16ae099eb3b77e5b8648e12a953
Garu006 <garojas@uamv.edu.ni>
2024-08-26 09:14:18 -0600

    Calcular raiz

---
+++ ejemplos/RaizCuadrada/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RaizCuadrada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            double raiz;
+            Console.WriteLine("Ingrese el numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            raiz=Math.Sqrt(num1);
+            Console.WriteLine("La raiz cuadrada de: " + num1 + " es: " + raiz);
+        }
+    }
+}
---
+++ ejemplos/RaizCuadrada/clases/RaizCuadrada.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaizCuadrada.clases
+{
+    internal class RaizCuadrada
+    {
+        int Num1 {  get; set; } 
+        double Raiz {  get; set; }
+        public int resultado()
+        {
+            return Num1;
+        }
+    }
+}
