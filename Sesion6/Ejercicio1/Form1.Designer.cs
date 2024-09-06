b19642d8a611fdc7659c7a656d91eb29a4fbf8bf
Garu006 <garojas@uamv.edu.ni>
2024-09-06 08:48:26 -0600

    Sesion6

---
+++ Sesion6/Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.cmbNombres = new System.Windows.Forms.ComboBox();
+            this.cmbCopiar = new System.Windows.Forms.ComboBox();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNombre.Location = new System.Drawing.Point(62, 52);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(69, 20);
+            this.lbNombre.TabIndex = 0;
+            this.lbNombre.Text = "Nombre:";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.ForeColor = System.Drawing.Color.Black;
+            this.tbNombre.Location = new System.Drawing.Point(137, 52);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(246, 20);
+            this.tbNombre.TabIndex = 1;
+            this.tbNombre.TextChanged += new System.EventHandler(this.tbNombre_TextChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.DimGray;
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Image = global::Ejercicio1.Properties.Resources.agregar;
+            this.btnAgregar.Location = new System.Drawing.Point(390, 38);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(45, 43);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // cmbNombres
+            // 
+            this.cmbNombres.FormattingEnabled = true;
+            this.cmbNombres.Location = new System.Drawing.Point(137, 88);
+            this.cmbNombres.Name = "cmbNombres";
+            this.cmbNombres.Size = new System.Drawing.Size(246, 21);
+            this.cmbNombres.TabIndex = 3;
+            // 
+            // cmbCopiar
+            // 
+            this.cmbCopiar.FormattingEnabled = true;
+            this.cmbCopiar.Location = new System.Drawing.Point(137, 181);
+            this.cmbCopiar.Name = "cmbCopiar";
+            this.cmbCopiar.Size = new System.Drawing.Size(246, 21);
+            this.cmbCopiar.TabIndex = 5;
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.Location = new System.Drawing.Point(137, 141);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(246, 23);
+            this.btnCopiar.TabIndex = 6;
+            this.btnCopiar.Text = "Mover todos los elementos";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 343);
+            this.Controls.Add(this.btnCopiar);
+            this.Controls.Add(this.cmbCopiar);
+            this.Controls.Add(this.cmbNombres);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.lbNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ComboBox cmbNombres;
+        private System.Windows.Forms.ComboBox cmbCopiar;
+        private System.Windows.Forms.Button btnCopiar;
+    }
+}
+
---
+++ Sesion6/Ejercicio1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nombre=tbNombre.Text;
+            cmbNombres.Items.Add(nombre);
+            tbNombre.Text = "";
+            tbNombre.Focus();
+        }
+
+        private void tbNombre_TextChanged(object sender, EventArgs e)
+        {
+            agregar();
+        }
+
+        public void agregar()
+        {
+            string nombre = tbNombre.Text.Trim();//trim borra los espacios en blanco al inicio y al final de un texto;
+            if (string.IsNullOrEmpty(nombre))
+            {
+                MessageBox.Show("No puede quedar vacio el nombre");
+                tbNombre = null;
+                return;
+            }
+            cmbNombres.Items.Add(nombre);
+            tbNombre.Text = "";
+            tbNombre.Focus();
+        }
+        private void cmdNombres2_SelectedIndexChange(object sender, EventArgs e)
+        {
+            int cant = cmbNombres.Items.Count;
+            for(int i =0; i<cant; i++)
+            {
+                string nombre=cmbNombres.Items[i].ToString();
+                cmbNombres.Items.Add(nombre);
+            }
+        }
+    }
+}
